5d9065d048c963d21f13a9ecf41b7c80ffefc959
Kris Balonek <kris@vyntelligence.com>
2024-12-12 17:34:17 +0000

    Try fetching the private IP from ECS metadata endpoint

--- app/app/settings/prod.py
+++ app/app/settings/prod.py
@@ -1,5 +1,10 @@
 """ Production Settings """
+import logging
+import requests
 from .stage import *
+
+
+logger = logging.getLogger(__name__)
 
 
 # Set to your Domain here
@@ -7,6 +12,12 @@ ALLOWED_HOSTS = [
     "testmaker.balonek.pl",
     "www.testmaker.balonek.pl",
 ]
-# The ALB uses the IP while calling the health check endpoint
+# The ALB uses the private IP when calling the health check endpoint
 if os.environ.get("AWS_EXECUTION_ENV"):
-    ALLOWED_HOSTS.append(gethostbyname(gethostname()))
+    METADATA_URI = os.environ['ECS_CONTAINER_METADATA_URI']
+    container_metadata = requests.get(METADATA_URI).json()
+    logging.info("Container metadata", container_metadata)
+    for network in container_metadata['Networks']:
+        for ip_v4_address in network['IPv4Addresses']:
+            ALLOWED_HOSTS.append(ip_v4_address)
+logger.info("ALLOWED_HOSTS", ALLOWED_HOSTS)
